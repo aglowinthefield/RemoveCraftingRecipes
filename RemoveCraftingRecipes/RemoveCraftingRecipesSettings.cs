e03787f1ef2cb3519d3f7dc83e0ebf778f937b34
aglowinthefield <tom.auty+nexus@gmail.com>
2023-09-25 12:32:52 -0400

    Initial commit

---
+++ RemoveCraftingRecipes/RemoveCraftingRecipesSettings.cs
@@ -0,0 +1,16 @@
+﻿using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Plugins.Records;
+using Mutagen.Bethesda.WPF.Reflection.Attributes;
+
+namespace RemoveCraftingRecipes;
+
+public class RemoveCraftingRecipesSettings
+{
+    [SettingName("Mods to trim")]
+    [Tooltip("These mods will have their crafting recipes removed")]
+    public HashSet<ModKey> ModsToTrim = new() { };
+
+    [SettingName("Remove tempering")]
+    [Tooltip("Also remove the tempering recipes if they exist")]
+    public bool RemoveTempering = false;
+}
